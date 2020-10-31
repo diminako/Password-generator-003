c0e224640f56a167f5553484f2a1d9d92c071aa4
DimitriNakos <dimi1nakos@gmail.com>
2020-10-31 15:53:31 -0400

    added comments into the js and cleaned up the logic slightly

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,25 @@
 // Assignment Code
+//Assigned variable to HTML element with id of generate
 var generateBtn = document.querySelector("#generate");
 
+//  Created a random number generator function
 function randoGen(x) {
   return Math.floor(Math.random() * x)
 }
 
+//  This function uses the random number generator to randomly choose a lower case character
 var lowerArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 function randomLower() {
   return lowerArr[randoGen(lowerArr.length)]
 }
 
+//  This function uses the random number generator to randomly choose a upper case character
 var upperArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 function randomUpper() {
   return upperArr[randoGen(upperArr.length)]
 }
 
+//  This function uses the random number generator to randomly choose between special characters in this array
 var specialChars = ["!", "@", "#", "$", "%", "&", "*", "?", "<", ">", "^", "="];
 function randomSpecial() {
   return specialChars[randoGen(specialChars.length)]
@@ -27,16 +32,22 @@ function writePassword() {
   passwordText.value = password;
 }
 
+//  created a function to generate the password used in the writePassword function
 function generatePassword() {
+
+  //  prompts and confirms to let the User input how they would like their password to be generated
   var passLengthPrompt = prompt("Please enter the passwords length: at least 8 and no more than 128 characters")
+  //  changes the User inputed password length to be a number
   var passLength = parseInt(passLengthPrompt);
   var ifLowerPrompt = confirm("If you would like lower case characters choose ok.  If not choose cancel.")
   var ifUpperPrompt = confirm("If you would like UPPER case characters choose ok.  If not choose cancel.")
   var ifNumPrompt = confirm("If you would like numerical characters choose ok.  If not choose cancel.")
   var numChars = randoGen(10)
   var ifSpecialPrompt = confirm("If you would like special characters choose ok.  If not choose cancel.")
+  //  An array to store the random characters for the password
   var genPass = []
 
+  //  If statement controlling the logic behind the characters inputs.
   if (passLength >= 8 && passLength <= 128) {
     for (var i = 1; passLength >= i;) {
       if (ifLowerPrompt) {
@@ -51,11 +62,13 @@ function generatePassword() {
       } else if (ifSpecialPrompt) {
         genPass.push(randomSpecial())
         i++
+        //  If selections are not valid ie. no options were picked to fill the password.  the function will reset.
       } else if (!ifLowerPrompt && !ifUpperPrompt && !ifNumPrompt && !ifSpecialPrompt) {
         alert("You must make at least 1 selection after choosing the password length.")
         generatePassword();
       }
     }
+    //  If the incorrect length was given by the User this resets the function over.
   } else {
     alert("You must choose a number at least 8 and no more than 128")
     generatePassword()
@@ -70,6 +83,8 @@ function generatePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+
+// My original logic which didn't follow the instructions well enough but still worked.
 
 // var specialChars = ["!", "@", "#", "$", "%", "&", "*", "?", "<", ">", "^", "="];
 // var specialChars1 = specialChars[randoGen(10)];
