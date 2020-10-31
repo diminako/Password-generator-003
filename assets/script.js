5b7598eb29b7b9fa5dfc23bbd4d9cdb7b7bf33e5
DimitriNakos <dimi1nakos@gmail.com>
2020-10-31 13:01:24 -0400

    password generator complete

--- assets/script.js
+++ assets/script.js
@@ -1,43 +1,34 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-
-
 // Write password to the #password input
 function writePassword() {
-
   var password = generatePassword();
-
   var passwordText = document.querySelector("#password");
-
-
-
   passwordText.value = password;
-
-
 }
-
-
-
-
 
 function generatePassword() {
-  var specialChars = ["!", "@", "#", "$", "%", "&"];
   var randoNum = Math.floor(Math.random() * 10)
+  var randoNum2 = Math.floor(Math.random() * 10)
+  var specialChars = ["!", "@", "#", "$", "%", "&", "*", "?", "<", ">", "^", "="];
+  var specialChars1 = specialChars[randoNum];
+  var specialChars2 = specialChars[randoNum2];
   var userName = prompt("Please enter your first name.")
+  var userAge = prompt("Please enter your birth year")
+  var favAnimal = prompt("Please enter your favorite animal.")
+  var favAnimalChar = favAnimal.slice(1, 3);
 
-
-
-
-
-
-  console.log("hello")
+  return userAge[0] +
+    userName[0] +
+    Math.floor(Math.random() * 10) +
+    specialChars1 +
+    favAnimalChar +
+    Math.floor(Math.random() * 10) +
+    userName[userName.length - 1] +
+    userAge[userAge.length - 1] +
+    specialChars2
 }
-
-
-
-
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
