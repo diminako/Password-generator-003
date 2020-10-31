0f8dfd3f1c66bdc19f6ba3221fef0b1c01c03a12
DimitriNakos <dimi1nakos@gmail.com>
2020-10-31 14:34:09 -0400

    changing my logic up to better fit the parameters

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function randoGen(x) {
+  return Math.floor(Math.random() * x)
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -9,26 +13,62 @@ function writePassword() {
 }
 
 function generatePassword() {
-  var randoNum = Math.floor(Math.random() * 10)
-  var randoNum2 = Math.floor(Math.random() * 10)
+  var passLengthPrompt = prompt("Please enter the passwords length: at least 8 and no more than 128 characters")
+  var passLength = parseInt(passLengthPrompt);
+  var ifLowerPrompt = confirm("If you would like lower case characters choose ok.  If not choose cancel.")
+  var ifUpperPrompt = confirm("If you would like UPPER case characters choose ok.  If not choose cancel.")
+  var lowerUpperArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  var ifNumPrompt = confirm("If you would like numerical characters choose ok.  If not choose cancel.")
+  var numChars = randoGen(10)
+  var ifSpecialPrompt = confirm("If you would like special characters choose ok.  If not choose cancel.")
   var specialChars = ["!", "@", "#", "$", "%", "&", "*", "?", "<", ">", "^", "="];
-  var specialChars1 = specialChars[randoNum];
-  var specialChars2 = specialChars[randoNum2];
-  var userName = prompt("Please enter your first name.")
-  var userAge = prompt("Please enter your birth year")
-  var favAnimal = prompt("Please enter your favorite animal.")
-  var favAnimalChar = favAnimal.slice(1, 3);
 
-  return userAge[0] +
-    userName[0] +
-    Math.floor(Math.random() * 10) +
-    specialChars1 +
-    favAnimalChar +
-    Math.floor(Math.random() * 10) +
-    userName[userName.length - 1] +
-    userAge[userAge.length - 1] +
-    specialChars2
+  if (passLength > 8 && passLength < 128) {
+    for (var i = 1; passLength >= i )
+  }
+
+
+
+
+
+
 }
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// var specialChars = ["!", "@", "#", "$", "%", "&", "*", "?", "<", ">", "^", "="];
+// var specialChars1 = specialChars[randoGen(10)];
+// var specialChars2 = specialChars[randoGen(10)];
+// var userName = prompt("Please enter your first name.", "Name")
+// var userAge = prompt("Please enter your birth year", "2020")
+// var favAnimal = prompt("Please enter your favorite animal.", "dog")
+// var passLengthPrompt = prompt("Please enter the passwords length: at least 8 and no more than 128 characters")
+// var passLength = parseInt(passLengthPrompt)
+
+// while (passLength > 8 || passLength < 128) {
+//   passLength = prompt("Your selection must be between 8 and 128 characters.  Failure to choose will automatically assign 10 characters.")
+// }
+
+
+
+
+// var generatedPass = userAge[randoGen(4)] +
+//   userName[randoGen(3)].toUpperCase() +
+//   randoGen(10) +
+//   specialChars1 +
+//   favAnimal[randoGen(favAnimal.length)] +
+//   randoGen(10) +
+//   favAnimal[randoGen(favAnimal.length)] +
+//   userName[userName.length - 1].toUpperCase() +
+//   userAge[randoGen(4)] +
+//   specialChars2;
+
+
+
+
+// console.log(generatedPass);
+// return generatedPass;
